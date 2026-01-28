5150f84980e18efc7333621e5de89593c500d344
Hugo Ramirez <121719045+hugoramirez93@users.noreply.github.com>
2026-01-28 17:42:57 -0500

    Delete Segunda-clase directory

--- Segunda-clase/tienda/prisma/migrations/20260124160520_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Producto" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "nombre" TEXT NOT NULL,
-    "descripcion" TEXT NOT NULL,
-    "precio" REAL NOT NULL,
-    "stock" INTEGER NOT NULL
-);
