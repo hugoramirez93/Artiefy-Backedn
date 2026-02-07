3c2eba704178b758f13e57b34d25e80e1be2165b
Hugoramirez93 <hugoo.ramirez@outlook.com>
2026-02-07 18:36:37 -0500

    Primera-clase corregido

--- Primera-clase/back-rest/src/app/api/login/route.ts
+++ Primera-clase/back-rest/src/app/api/login/route.ts
@@ -1,17 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  const { email, password } = await request.json();
-
-  const validUsername = "admin";
-  const validPassword = "1234";
-
-  if (email === validUsername && password == validPassword) {
-    return NextResponse.json({ message: "Login successful" }, { status: 200 });
-  } else {
-    return NextResponse.json(
-      { message: "Invalid email or password" },
-      { status: 401 },
-    );
-  }
+export async function GET() {
+  const nameApi = process.env.NOMBRE_API || "API";
+  return NextResponse.json({ message: `Hello login ${nameApi}!` });
 }
